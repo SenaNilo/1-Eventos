a078beafe202647fc3787ade453e1e0da63dc300
Danilo Sena Santos <98361804+SenaNilo@users.noreply.github.com>
2024-05-31 17:50:59 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,25 @@
+//1. a)
+function clkP(){
+    document.querySelectorAll("p").forEach(elem => {
+        elem.addEventListener("click", () => {
+            elem.innerHTML = "Parágrafo clicado";
+        });
+        elem.addEventListener("dblclick", () => {
+            elem.innerHTML = Parágrafo duplo-clicado";
+        })
+    })
+}
+
+//1. b)
+function hideMe(){
+    var btn = document.querySelector("#b2");
+    var hides = document.querySelectorAll(".hideMe");
+    btn.addEventListener("click", () => {
+        hides.forEach(elem => {
+            elem.style.display = "none";
+        })
+    })
+}
+
+window.onload = hideMe;
+window.onload = clkP;
